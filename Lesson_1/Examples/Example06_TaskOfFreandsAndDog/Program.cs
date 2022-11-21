c94933729ec38bad24e4bbce86b20b8ca9dbc265
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2022-11-21 13:42:28 +0700

    adding of task about DOG

---
+++ Lesson_1/Examples/Example06_TaskOfFreandsAndDog/Program.cs
@@ -0,0 +1,22 @@
+﻿int countOfRunsOfDog = 0;
+double distance = 1000;
+double firstFriendSpeed = 4;
+double secondFriendSpeed = 5;
+double dogSpeed = 25;
+bool directionToSecondFriend = true;
+
+
+while (distance > 10) 
+{
+    double time;
+    if (directionToSecondFriend) {
+        time = distance / (dogSpeed + secondFriendSpeed);
+    } else {
+        time = distance / (dogSpeed + firstFriendSpeed);
+    }
+    directionToSecondFriend = !directionToSecondFriend;
+    distance -= (firstFriendSpeed + secondFriendSpeed) * time;
+    countOfRunsOfDog ++;
+}
+
+Console.WriteLine("Our dog was run between friends " + countOfRunsOfDog + " times!");
