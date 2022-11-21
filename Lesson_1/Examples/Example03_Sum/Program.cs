c105bcedbf226e46cb9bbd44b4bfeae0a6cf16ab
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2022-11-21 11:19:52 +0700

    adding of summing

---
+++ Lesson_1/Examples/Example03_Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = new Random().Next(1, 20);
+Console.Write("первое число =  ");
+Console.WriteLine(a);
+
+int b = new Random().Next(1, 20);
+Console.Write("второе число = ");
+Console.WriteLine(b);
+
+Console.Write("Сумма ваших чисел равна: ");
+Console.WriteLine(a + b);
