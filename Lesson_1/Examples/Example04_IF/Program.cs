82d9a20b1b55682be0cb5cfcba81ad36ddd6a1bb
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2022-11-21 11:21:18 +0700

    adding of operator IF

---
+++ Lesson_1/Examples/Example04_IF/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Input name of user!");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "alex") {
+    Console.WriteLine("O, Aleksandr, welcome!");
+} else {
+    Console.Write(username);
+    Console.WriteLine("Good day!"); 
+}
+
